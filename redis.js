50612bc35890d93c9896f24384570a4e1860b53d
Randall Nickerson <webshooter@gmail.com>
2021-03-19 13:46:03 -0700

    add REDIS_URL env to redisClient init

--- redis.js
+++ redis.js
@@ -1,6 +1,6 @@
 const redis = require("redis");
 
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({ url: process.env.REDIS_URL });
 
 redisClient.on("error", (err) => {
   // eslint-disable-next-line no-console
